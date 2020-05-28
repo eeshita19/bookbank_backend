44b292cb66a558007950f8c9b5bdcec5ac0ccd7a
eeshita19 <eeshita.gupta1909@gamil.com>
2020-05-29 00:22:11 +0530

    frontend

---
+++ functions/config/js/enterotp.js
@@ -0,0 +1,10 @@
+function movecursor(moveFrom,moveTo){
+    var length = moveFrom.value.length;
+    var maxlength = moveFrom.getAttribute("maxlength");
+    
+    if(length == maxlength)
+        {
+            document.getElementById(moveTo).focus();
+        }
+}
+
---
+++ functions/config/js/verification.js
@@ -0,0 +1,35 @@
+var selected = document.querySelector(".selected");
+var listitem = document.querySelector("#country");
+var optionscontainer = document.querySelector(".options-container");
+var optionslist = document.querySelectorAll(".options");
+
+selected.addEventListener("click", function(){
+optionscontainer.classList.toggle("active");    
+});
+
+optionslist.forEach(function(option){
+  option.addEventListener("click",function(){
+      listitem.innerHTML = option.querySelector("label").innerHTML;
+      optionscontainer.classList.remove("active");
+  });  
+});
+
+function dofirst(){
+    var button = document.getElementById("getotp")
+    window.addEventListener("click", saveinfo , false);
+
+}
+function saveinfo(){
+    var one;
+    var two = document.getElementById("inputphoneno").value;
+    sessionStorage.setItem(one,two);
+    window.console.log(sessionStorage);
+    
+//    display(one);
+//    window.console.log(display);
+}
+//function display(one){
+// var two = sessionStorage.getItem(one);
+//listitem.innerHTML = two;
+//}
+window.addEventListener("load", dofirst , false);
