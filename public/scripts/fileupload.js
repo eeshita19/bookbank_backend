35b15fdf4956330f3491a14ec2f6284cec12b56f
saqsham <saqsham@gmail.com>
2020-06-03 14:50:02 +0530

    ctrl/scripts

--- public/scripts/fileupload.js
+++ public/scripts/fileupload.js
@@ -1,12 +1,28 @@
 // uploading the files
 const ref = firebase.storage().ref();
-const file = document.querySelector('#photo').files[0]
+const file1 = document.querySelector('#photo1').files[0]
+const file2 = document.querySelector('#photo2').files[0]
+
 const name = (+new Date()) + '-' + file.name;
-const metadata = {
-    contentType: file.type
+const metadata1 = {
+    contentType: file1.type
 };
 
-const task = ref.child(name).put(file, metadata);
+const metadata2 = {
+    contentType: file2.type
+};
+
+const task = ref.child(name).put(file1, metadata1);
+task
+    .then(snapshot => snapshot.ref.getDownloadURL())
+    .then((url) => {
+        console.log(url);
+        document.querySelector('#someImageTagID').src = url;
+    })
+    .catch(console.error);
+
+
+const task = ref.child(name).put(file2, metadata2);
 task
     .then(snapshot => snapshot.ref.getDownloadURL())
     .then((url) => {
